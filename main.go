6421b8a2da60852b4bed02232dc8f649e0224253
rhymald <rhymald@gmail.com>
2022-12-31 20:35:38 +0100

    + Single foe spawn From random

--- main.go
+++ main.go
@@ -37,16 +37,17 @@ type Player struct {
 }
 
 var You Player
-var Foe Player
+var Target Player
 
 func init() {
-  fmt.Println("Initializing...")
+  fmt.Println("[Initializing...]")
   You = PlayerBorn(1)
   return
 }
 
 func main() {
-  fmt.Println("Here we go!")
+  fmt.Println("[Go!..]")
+  FoeSpawn(4)
   return
 }
 
@@ -54,9 +55,10 @@ func PlayerBorn(mean float64) Player {
   playerTuple := [][]string{}
   buffer := Player{}
   fmt.Println("Player creation start:")
-  buffer.Health.Max = mean*mean*100 // from db
+  buffer.Health.Max = (mean/100+1)*(mean/100+1)*50 // from db
   buffer.Health.Current = math.Sqrt(buffer.Health.Max+1)-1 //from db
-  current := fmt.Sprintf("Health|Current: %0.1f|Max: %0.1f|Rate: %1.1f%%", buffer.Health.Current, buffer.Health.Max, 100*buffer.Health.Current/buffer.Health.Max)
+  // current := fmt.Sprintf("Health|Current: %0.0f|Max: %0.0f|Rate: %1.0f%%", buffer.Health.Current, buffer.Health.Max, 100*buffer.Health.Current/buffer.Health.Max)
+  current := fmt.Sprintf("Health|Max: %0.0f|Current: %0.0f|Rate: %1.0f%%", buffer.Health.Max, buffer.Health.Current, 100*buffer.Health.Current/buffer.Health.Max)
   playerTuple = AddRow(current, playerTuple)
   buffer.Nature.Stream.Length  = 1+Rand()
   buffer.Nature.Stream.Width   = 1+Rand()
@@ -66,8 +68,41 @@ func PlayerBorn(mean float64) Player {
   buffer.Nature.Stream.Width  *= stabilizer
   buffer.Nature.Stream.Power  *= stabilizer
   buffer.Nature.Stream.Element = "Common"
+  playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
   row := fmt.Sprintf(
-    "%s|Cre: %0.3f|Alt: %0.3f|Des: %0.3f",
+    "%s|%0.3f|%0.3f|%0.3f",
+    buffer.Nature.Stream.Element,
+    buffer.Nature.Stream.Length,
+    buffer.Nature.Stream.Width,
+    buffer.Nature.Stream.Power,
+  )
+  playerTuple = AddRow(row,playerTuple)
+  thickness := math.Pi / ( 1/buffer.Nature.Stream.Power + 1/buffer.Nature.Stream.Width + 1/buffer.Nature.Stream.Length)
+  buffer.Nature.Pool.Max = math.Sqrt( thickness *1024 + 1024) - 1
+  playerTuple = AddRow( fmt.Sprintf("Pool|Max: %0.0f|Current: %d|Rate: %1.0f%%", buffer.Nature.Pool.Max, len(buffer.Nature.Pool.Dots), 100*float64(len(buffer.Nature.Pool.Dots))/float64(buffer.Nature.Pool.Max) ) ,playerTuple)
+  PlotTable(playerTuple)
+  return buffer
+}
+
+func FoeSpawn(mean float64) Player {
+  playerTuple := [][]string{}
+  buffer := Player{}
+  fmt.Println("Foe spawning start:")
+  buffer.Health.Max = (mean/100+1)*(mean/100+1)*50 // from db
+  buffer.Health.Current = buffer.Health.Max //from db
+  current := fmt.Sprintf("Health|||Rate: %1.0f%%", 100*buffer.Health.Current/buffer.Health.Max)
+  playerTuple = AddRow(current, playerTuple)
+  buffer.Nature.Stream.Length  = 1+Rand()
+  buffer.Nature.Stream.Width   = 1+Rand()
+  buffer.Nature.Stream.Power   = 1+Rand()
+  stabilizer := mean/Vector(buffer.Nature.Stream.Length, buffer.Nature.Stream.Width, buffer.Nature.Stream.Power)
+  buffer.Nature.Stream.Length *= stabilizer
+  buffer.Nature.Stream.Width  *= stabilizer
+  buffer.Nature.Stream.Power  *= stabilizer
+  buffer.Nature.Stream.Element = "Common"
+  playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
+  row := fmt.Sprintf(
+    "%s|%0.3f|%0.3f|%0.3f",
     buffer.Nature.Stream.Element,
     buffer.Nature.Stream.Length,
     buffer.Nature.Stream.Width,
@@ -75,7 +110,7 @@ func PlayerBorn(mean float64) Player {
   )
   playerTuple = AddRow(row,playerTuple)
   buffer.Nature.Pool.Max = math.Sqrt(buffer.Nature.Stream.Length*1024 + 1024) - 1
-  playerTuple = AddRow( fmt.Sprintf("Pool|Current %d|Max: %0.0f|Rate: %1.1f%%", len(buffer.Nature.Pool.Dots), buffer.Nature.Pool.Max, 100*float64(len(buffer.Nature.Pool.Dots))/float64(buffer.Nature.Pool.Max) ) ,playerTuple)
+  playerTuple = AddRow( fmt.Sprintf("Pool|Max: %0.0f", buffer.Nature.Pool.Max ) ,playerTuple)
   PlotTable(playerTuple)
   return buffer
 }
@@ -106,41 +141,43 @@ func PlotTable(tuple [][]string) {
     }
   }
   //Head:
-  fmt.Printf("╔")
+  fmt.Printf(" ╔")
   for i, wid := range maxs {
     for counter:=0 ;counter < wid; counter++ {
       fmt.Printf("═")
     }
-    if i+1 == len(maxs) {fmt.Printf("╗\n")} else {fmt.Printf("╤")}
+    if i+1 == len(maxs) {fmt.Printf("╗\n")} else {fmt.Printf("╤")} //╤
   }
   //String:
   for I, _ := range tuple {
-    fmt.Printf("║")
+    fmt.Printf(" ║")
     for i, wid := range maxs {
       fmt.Printf(" ")
       fmt.Printf("%s", tuple[I][i])
       for counter:=0 ;counter < wid-1-len(tuple[I][i]); counter++ {
         fmt.Printf(" ")
       }
-      if i+1 == len(maxs) {fmt.Printf("║\n")} else {fmt.Printf("│")}
+      if i+1 == len(maxs) {fmt.Printf("║\n")} else {
+        if tuple[I][i+1]=="" || tuple[I][i+1]==" " {fmt.Printf("│")} else {fmt.Printf("│")}
+      }
     }
     if I+1 == len(tuple) {
       //Footer:
-      fmt.Printf("╚")
+      fmt.Printf(" ╚")
       for i, wid := range maxs {
         for counter:=0 ;counter < wid; counter++ {
           fmt.Printf("═")
         }
-        if i+1 == len(maxs) {fmt.Printf("╝\n")} else {fmt.Printf("╧")}
+        if i+1 == len(maxs) {fmt.Printf("╝\n")} else {fmt.Printf("╧")} //╧
       }
     } else {
       //Delimiter:
-      fmt.Printf("╟")
+      fmt.Printf(" ╟")
       for i, wid := range maxs {
         for counter:=0 ;counter < wid; counter++ {
           fmt.Printf("─")
         }
-        if i+1 == len(maxs) {fmt.Printf("╢\n")} else {fmt.Printf("┼")}
+        if i+1 == len(maxs) {fmt.Printf("╢\n")} else {fmt.Printf("┼")} //┼
       }
     }
   }
@@ -149,9 +186,9 @@ func AddRow(row string, tuple [][]string) [][]string {
   buffer := strings.Split(row, "|")
   if len(tuple)==0 { return [][]string{buffer} }
   if len(buffer) > len(tuple[0]) {
-    for l, _ := range tuple { for count := 0; count < len(buffer)-len(tuple[0]); count++ { tuple[l] = append(tuple[l], "") } }
+    for l, _ := range tuple { for count := 0; count < len(buffer)-len(tuple[0]); count++ { tuple[l] = append(tuple[l], " ") } }
   } else if len(buffer) < len(tuple[0]) {
-    for count := 0; count < len(tuple[0])-len(buffer); count++ { buffer = append(buffer, "") }
+    for count := 0; count <= len(tuple[0])-len(buffer); count++ { buffer = append(buffer, " ") }
   }
   tuple = append(tuple, buffer)
   return tuple
