22294f53c48c95fcecdbbfc6ecd4f0e666806dd1
rhymald <rhymald@gmail.com>
2023-01-02 00:11:13 +0100

    ~ Multiline tuple plot

--- main.go
+++ main.go
@@ -79,9 +79,9 @@ func PlayerBorn(mean float64) Player {
   buffer.Nature.Stream.Alt  *= stabilizer
   buffer.Nature.Stream.Des  *= stabilizer
   buffer.Nature.Stream.Element = "Common"
-  playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
+  // playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
   row := fmt.Sprintf(
-    "%s|%0.3f|%0.3f|%0.3f",
+    "Element\n%s|Creation\n%0.3f|Alteration\n%0.3f|Destruction\n%0.3f",
     buffer.Nature.Stream.Element,
     buffer.Nature.Stream.Cre,
     buffer.Nature.Stream.Alt,
@@ -91,7 +91,7 @@ func PlayerBorn(mean float64) Player {
   thickness := math.Pi / ( 1/buffer.Nature.Stream.Des + 1/buffer.Nature.Stream.Alt + 1/buffer.Nature.Stream.Cre)
   buffer.Nature.Pool.Max = math.Sqrt( thickness *1024 + 1024) - 1
   playerTuple = AddRow( fmt.Sprintf("Pool|Max: %0.0f|Current: %d|Rate: %1.0f%%", buffer.Nature.Pool.Max, len(buffer.Nature.Pool.Dots), 100*float64(len(buffer.Nature.Pool.Dots))/float64(buffer.Nature.Pool.Max) ) ,playerTuple)
-  PlotTable(playerTuple, true)
+  PlotTable(playerTuple, false)
   return buffer
 }
 
@@ -111,9 +111,9 @@ func FoeSpawn(mean float64) Player {
   buffer.Nature.Stream.Alt  *= stabilizer
   buffer.Nature.Stream.Des  *= stabilizer
   buffer.Nature.Stream.Element = "Common"
-  playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
+  // playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
   row := fmt.Sprintf(
-    "%s|%0.3f|%0.3f|%0.3f",
+    "Element\n%s|Creation\n%0.3f|Alteration\n%0.3f|Destruction\n%0.3f",
     buffer.Nature.Stream.Element,
     math.Sqrt(mean*mean/3),
     math.Sqrt(mean*mean/3),
@@ -134,9 +134,9 @@ func PlayerStatus(it Player) {
   fmt.Println("Player status:")
   current := fmt.Sprintf("Health|Max: %0.0f|Current: %0.0f|Rate: %1.0f%%", it.Health.Max, it.Health.Current, 100*it.Health.Current/it.Health.Max)
   playerTuple = AddRow(current, playerTuple)
-  playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
+  // playerTuple = AddRow("Element|Creation|Alteration|Destruction",playerTuple)
   row := fmt.Sprintf(
-    "%s|%0.3f|%0.3f|%0.3f",
+    "Element\n%s|Creation\n%0.3f|Alteration\n%0.3f|Destruction\n%0.3f",
     it.Nature.Stream.Element,
     it.Nature.Stream.Cre,
     it.Nature.Stream.Alt,
@@ -145,7 +145,6 @@ func PlayerStatus(it Player) {
   playerTuple = AddRow(row,playerTuple)
   playerTuple = AddRow( fmt.Sprintf("Pool|Max: %0.0f|Current: %d|Rate: %1.0f%%", it.Nature.Pool.Max, len(it.Nature.Pool.Dots), 100*float64(len(it.Nature.Pool.Dots))/float64(it.Nature.Pool.Max) ) ,playerTuple)
   PlotTable(playerTuple, false)
-
 }
 
 func Rand() float64 {
@@ -168,9 +167,9 @@ func PlotTable(tuple [][]string, stretch bool) {
   for j, y := range tuple {
     for i, _ := range y {
       if j == 0 {
-        for c := 0; c>len(maxs); c++ { maxs[i] = 2+len(tuple[0][i]) }
+        for c := 0; c>len(maxs); c++ { maxs[i] = 2+ MaxInCell(tuple[0][i]) }
       }
-      maxs[i] = int(math.Max(2+float64(len(tuple[j][i])), float64(maxs[i])))
+      maxs[i] = int(math.Max(2+float64( MaxInCell(tuple[j][i]) ), float64(maxs[i])))
     }
   }
   if stretch {
@@ -190,18 +189,8 @@ func PlotTable(tuple [][]string, stretch bool) {
     if i+1 == len(maxs) {fmt.Printf("╗\n")} else {fmt.Printf("╤")} //╤
   }
   //String:
-  for I, _ := range tuple {
-    fmt.Printf(" ║")
-    for i, wid := range maxs {
-      fmt.Printf(" ")
-      fmt.Printf("%s", tuple[I][i])
-      for counter:=0 ;counter < wid-1-len(tuple[I][i]); counter++ {
-        fmt.Printf(" ")
-      }
-      if i+1 == len(maxs) {fmt.Printf("║\n")} else {
-        if tuple[I][i+1]=="" || tuple[I][i+1]==" " {fmt.Printf("│")} else {fmt.Printf("│")}
-      }
-    }
+  for I, row := range tuple {
+    PlotRow(row, maxs)
     if I+1 == len(tuple) {
       //Footer:
       fmt.Printf(" ╚")
@@ -233,6 +222,36 @@ func AddRow(row string, tuple [][]string) [][]string {
   }
   tuple = append(tuple, buffer)
   return tuple
+}
+func FindDelim(row []string) ([]int, int) {
+  buffer := make([]int, len(row))
+  max := 0
+  for i, cell := range row { buffer[i] = len(strings.Split(cell, "\n")) ; max = int(math.Max( float64(buffer[i]), float64(max) )) }
+  return buffer, max
+}
+func PlotRow(row []string, widths []int) {
+  _, max := FindDelim(row)
+  for linenum:=0; linenum<max; linenum++ {
+    fmt.Printf(" ║")
+    for i, wid := range widths {
+      fmt.Printf(" ")
+      cell := strings.Split(row[i], "\n")
+      if len(cell) < max { for count:=0; count<max-len(cell); count++ { cell = append(cell, string(" ")) } }
+      toprint := cell[linenum]
+      fmt.Printf("%s", toprint)
+      for counter:=0 ;counter < wid-1-len(toprint); counter++ {
+        fmt.Printf(" ")
+      }
+      if i+1 == len(widths) {fmt.Printf("║\n")} else {
+        if row[i+1]=="" || row[i+1]==" " {fmt.Printf("│")} else {fmt.Printf("│")}
+      }
+    }
+  }
+}
+func MaxInCell(cell string) int {
+  lines, max := strings.Split(cell, "\n"), 0
+  for _, each := range lines { max = int(math.Max( float64(max), float64(len(each)) )) }
+  return max
 }
 
 func Regeneration(pool *[]Dot, health *float64, max float64, maxhp float64, stream Stream) {
